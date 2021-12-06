45c996abdff6e44cfe82fbbe4c55b2c46e14c00e
NikP <NikP@DESKTOP-R65VTUR>
2021-12-06 12:16:46 +0300

    Добавьте файлы проекта.

---
+++ TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+one();
+two();
+void one()
+{
+    Console.WriteLine("Test");
+}
+void two()
+{
+    Console.WriteLine("Test2");
+}
